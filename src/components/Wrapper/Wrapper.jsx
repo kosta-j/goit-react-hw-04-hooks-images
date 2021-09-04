59feff84ddb88c53d6bfd876a63722ed69937cec
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-09-04 13:53:31 +0300

    add Wrapper refactoring with hooks

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,28 +1,24 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import Searchbar from '../Searchbar/Searchbar';
 import s from './Wrapper.module.css';
 
-class Wrapper extends Component {
-  state = {
-    query: '',
+function Wrapper() {
+  const [query, setQuery] = useState('');
+
+  const updateQuery = text => {
+    setQuery(text);
   };
 
-  updateQuery = text => {
-    this.setState({ query: text });
-  };
-
-  render() {
-    return (
-      <div className={s.wrapper}>
-        <Searchbar onSubmit={this.updateQuery} />
-        <ImageGallery query={this.state.query} />
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
+  return (
+    <div className={s.wrapper}>
+      <Searchbar onSubmit={updateQuery} />
+      <ImageGallery query={query} />
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
 }
 
 export default Wrapper;
